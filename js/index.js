f740b1b532762696e5d766b4de405c6e442389b8
serega.starshov <serega.starshov@mail.ru>
2021-09-15 21:25:02 +0300

    add documents

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+import documents from "./modules/documents";
 import requestCall from "./modules/modalCall";
 import showSliderProfits from "./modules/sliderProfits";
 import timer from "./modules/timer";
@@ -12,3 +13,6 @@ showSliderProfits();
 
 //=========timer=====================
 timer('21 September 2021');
+
+//========documents==================
+documents();
